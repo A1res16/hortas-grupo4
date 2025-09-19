0fd355d86dd027ea286df0db2d668a90120ffcc8
Alexandre Cruz <1221585@isep.ipp.pt>
2025-09-19 14:05:07 +0100

    commit inical da classe Horta

--- Lab2_grupo4/src/Horta.java
+++ Lab2_grupo4/src/Horta.java
@@ -1,5 +1,49 @@
 
-public class Horta 
-{
-   
+public class Horta {
+	Private String nome;
+	Private String loc;
+	
+	public horta(String nome; String loc) {
+		this.nome = nome;
+		this.loc = loc;
+		private List<Talhao> talhoes;
+		}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getLoc() {
+		return loc;
+	}
+	
+	public List<Talhao> getTalhoes() {
+        return talhoes;
+    }
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setLoco(String loc) {
+		this.loc = loc;
+	}
+	
+	public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+	
+	public boolean adicionarTalhao(Talhao novoTalhao) {
+		
+		for (Talhao t : talhoes) {
+			if (t.getID( == novoTalhao.getID())) {
+				System.out.println("O Talhão com ID: " + novoTalhao.getID() + " já existe");
+				return false;
+			}
+			talhoes.add(novoTalhao);
+			System.out.println("O Talhão com ID: " + novoTalhao.getID() + " foi adicionado com sucesso!");
+			return true;
+		}
+	}
+	
 }
